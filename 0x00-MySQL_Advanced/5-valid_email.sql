60fcb14157553a25bb227c8c00c03bf39702dfef
thevorgx <kh.elallali@gmail.com>
2024-08-08 14:14:28 +0100

    update

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,4 +3,4 @@
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-    SET NEW.valid_email = IF(OLD.email != NEW.email, DEFAULT, NEW.valid_email);
+    SET NEW.valid_email = IF(OLD.email != NEW.email, 0, NEW.valid_email);
