3b5a5a5c9a69847fd245cba400ea1d6cdbd40e56
thevorgx <kh.elallali@gmail.com>
2024-08-12 21:47:54 +0100

    task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""find docs"""
+
+
+def list_all(mongo_collection):
+    """find all documents in a collection"""
+    doc_list = list(mongo_collection.find())
+    return (doc_list)
