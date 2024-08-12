584b1ad677d3a027d1b8fd4a0e257a3bb45007ae
thevorgx <kh.elallali@gmail.com>
2024-08-12 22:47:21 +0100

    task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""schools by a topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """return a list of schools by a topic"""
+    by_topic = {"topics": topic}
+    schools_list = list(mongo_collection.find(by_topic))
+    return (schools_list)
