df6b9364e48ab9826621e37e49167cb95ea087d7
thevorgx <kh.elallali@gmail.com>
2024-08-13 15:01:01 +0100

    advanced 101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""for future Vorg:
+aggregation are on 2 steps:
+1- assemble name and create a new  temporary field named averageScore
+where scores of each topic are added and averaged using $avg.
+2- sort the average score using $sort, -1 means from top to bottom
+ps: even the id is needed on the output didn't include it on the stage 1
+because in Mongodb it's included by default."""
+
+
+def top_students(mongo_collection):
+    """return students with their avg score(descending order)"""
+    aggregation_stages = [
+        {
+            "$project": {
+                "name": 1,
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+
+    top_studs = list(mongo_collection.aggregate(aggregation_stages))
+
+    return (top_studs)
