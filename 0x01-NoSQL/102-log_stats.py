f2c476ec687aa1dafcc6518750a0e115b3480473
thevorgx <kh.elallali@gmail.com>
2024-08-13 20:43:11 +0100

    task 102 (not finished yet)

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Log stats"""
+import pymongo
+
+
+if __name__ == "__main__":
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    count_logs = nginx_collection.count_documents({})
+    print(f"{count_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        method_count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    status_check_count = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
