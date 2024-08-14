99ae4eba5b65de84771aff773effa6c0f3d1c432
thevorgx <kh.elallali@gmail.com>
2024-08-14 16:45:13 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Writing strings to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -23,3 +23,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return (key)
+
+    def get(self, key: str, fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float]:
+        """get key"""
+        get_key = self._redis.get(key)
+        if get_key is None:
+            return(None)
+        if fn:
+            return (fn(get_key))
+        return (get_key)
+
+    def get_str(self, key: str) -> str:
+        """get key as str"""
+        key_to_convert = self.get(key, str)
+        return(key_to_convert)
+
+    def get_int(self, key: int) -> int:
+        """get key as int"""
+        key_to_convert = self.get(key, int)
+        return (key_to_convert)
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -6,11 +6,33 @@ import redis
 
 Cache = __import__('exercise').Cache
 
-cache = Cache()
+def vorg_tests():
+    """Run test cases for the Cache class."""
+    cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
+    print("All vorg tests passed")
+
+def main():
+    """Main function to run the cache example and tests."""
+    cache = Cache()
+
+    data = b"hello"
+    key = cache.store(data)
+    print(f"Stored key: {key}")
+
+    local_redis = redis.Redis()
+    print(f"Data from Redis: {local_redis.get(key)}")
+
+    vorg_tests()
+
+if __name__ == "__main__":
+    main()
